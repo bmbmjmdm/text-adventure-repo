31aa98e75ca3130064fd663357bd654d44309508
bmbmjmdm <bmbmjmdm@gmail.com>
2018-10-18 01:15:14 -0400

    update ComingSoon

--- text-adventure-app/Menus/ComingSoon.js
+++ text-adventure-app/Menus/ComingSoon.js
@@ -4,9 +4,9 @@ import {OpenAppStore} from './OpenAppStore.js'
 export class ComingSoon {
 
 	static createPage(that){
-		that.preparePage("Coming Soon! I'm so happy you enjoy my work. If you'd like more levels, ")
+		that.preparePage("Coming Soon! I'm so happy you enjoy my work :). If you'd like more levels, ")
 		that.preparePage("give me a nice rating", OpenAppStore);
-		that.preparePage(" to motivate me to write more! You can also leave a review stating which new level you want most :)");
+		that.preparePage(" to motivate me to write more! Also be sure to turn on auto updates for iAdventure so that you get new levels when they come out.");
 		
 	}
 
